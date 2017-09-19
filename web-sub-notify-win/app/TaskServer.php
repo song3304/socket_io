49b7ae1854345c3ad24cfb287e24c08d484258ff
xp <xue_yun@foxmail.com>
2017-09-19 15:18:43 +0800

    组装数据

--- web-sub-notify-win/app/TaskServer.php
+++ web-sub-notify-win/app/TaskServer.php
@@ -14,6 +14,7 @@ use App\ClientWorker;
 use GatewayWorker\Lib\DbConnection;
 use App\msg\QuoteClass;
 use App\MsgIds;
+
 /**
  * Description of TaskServer
  * 定期从数据库中读取当前数据，并实时上报给消息中心
@@ -53,28 +54,77 @@ class TaskServer extends Worker {
             $this->emit();
         });
     }
-    
+
     /*
      * 组装成发给服务中心的数据
      */
-    private function msgData($record) {
-        $roomId = SubNotifyRooms::roomId(0, $record['product_id'], $record['user_id']);
+
+    private function msgData($product_id, $user_id, $record) {
+
+        $roomId = SubNotifyRooms::roomId(0, $product_id, $user_id);
+        //将array中的key去掉
+        $tmp = [];
+        foreach ($record as $key => $value) {
+            $tmp[$key] = array_values($value);
+        }
         $data = array(
             'id' => MsgIds::MESSAGE_GATEWAY_TO_GROUP,
             'room' => $roomId,
-            'data' => $record,
+            'data' => $tmp,
         );
         return $data;
     }
 
-
     /*
      * 将数据推送出去
      */
+
     protected function emit() {
-        foreach ($this->records as $record) {
-            $json = $this->msgData($record);
-            $this->client_worker->sendToGateway($json);
+        foreach ($this->records as $product_id => $records) {
+            // 结构：品类id->撮合id->类型->信息记录id
+            foreach ($records as $user_id => $record) {
+                //这一层是记录类型
+                $json = $this->msgData($product_id, $user_id, $record);
+                $this->client_worker->sendToGateway($json);
+                //发送之后将成交记录删除，即成交记录一直只发最新的
+                unset($this->records[$product_id][$user_id]['order']);
+            }
+        }
+    }
+
+    private function storeRecords($new_records) {
+        $time = time();
+        foreach ($new_records as $record) {
+            $record['server_timestamp'] = $time;
+            //保存  结构：品类id->撮合id->类型->信息记录id
+            $product_id = $record['product_id'];
+            $user_id = $record['user_id'];
+            $trade_type = $record['trade_type'];
+            $id = $record['id'];
+            switch ($trade_type) {
+                case -1:
+                    $trade_type = 'sell';
+                    break;
+                case 1:
+                    $trade_type = 'buy';
+                    break;
+                case 2:
+                    $trade_type = 'order';
+                    break;
+                default:
+                    break;
+            }
+            
+            if (!empty($record['delete_time']) && isset($this->records[$product_id][$user_id][$trade_type][$id])) {
+                //删除了
+                unset($this->records[$product_id][$user_id][$trade_type][$id]);
+            } else if (!in_array($trade_type, ['sell', 'buy', 'order'], TRUE)) {
+                //不是买、卖、成交记录
+                continue;
+            } else {
+                var_dump($trade_type);
+                $this->records[$product_id][$user_id][$trade_type][$id] = $record;
+            }
         }
     }
 
@@ -82,23 +132,17 @@ class TaskServer extends Worker {
      * 将记录保存在内存中
      */
 
-    protected function updateRecords($first_readdb = false /*首次读取数据*/) {
+    protected function updateRecords($first_readdb = false /* 首次读取数据 */) {
         //将当前时间保存下来
         $timestamp = $this->timestamp;
         $this->timestamp = time();
-        
-        $new_records = $first_readdb?$this->selectAllRecords():$this->selectAllRecordsAccordingTimestamp($timestamp);
+
+        $new_records = $first_readdb ? $this->selectAllRecords() : $this->selectAllRecordsAccordingTimestamp($timestamp);
         if (empty($new_records)) {
             //没有新数据
             return;
-        }
-        foreach ($new_records as $record) {
-            if (!empty($record['delete_time']) && isset($this->records[$record['id']])) {
-                //删除了
-                unset($this->records[$record['id']]);
-            } else {
-                $this->records[$record['id']] = $record;
-            }
+        } else {
+            $this->storeRecords($new_records);
         }
     }
 
@@ -110,7 +154,7 @@ class TaskServer extends Worker {
         //根据时间进行查询，仅仅查询比上次查询时间更晚的记录
         return $this->db->query("select * from en_product_offer where update_time>=$timestamp and delete_time>=$timestamp");
     }
-    
+
     /*
      * 查找所有记录
      */
